19200c4dabf66b8a938a02a87235840cf008efd8
Zuzanna <84448044+zuzanna-m-j@users.noreply.github.com>
2026-04-01 17:14:52 -0500

    Create cudaHW1.cu

---
+++ Homework/cudaHW1.cu
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+__global__ void kernel(int *in_data, int *out_data, int *out_tid, int N)
+{   
+    
+/// Write the kernel
+
+
+}
+
+
+int main()
+{
+    // Array size
+    const int N = 96;
+    size_t bytes = N * sizeof(int);
+
+    // Timing variables
+        
+    cudaError_t err;
+
+    // Host arrays
+    int *in_data = (int*)malloc(2*bytes);
+    int *out_data = (int*)malloc(bytes);
+    int *out_tid = (int*)malloc(bytes);
+    
+    // Initialize input array on host
+    for (int i = 0; i < 2*N; i++)
+    {
+        in_data[i] = i;
+    }
+    
+    // Device arrays
+    int *d_in_data, *d_out_data, *d_out_tid;
+    
+    // Allocate memory on the device
+    cudaMalloc(&d_in_data, 2* bytes);
+    cudaMalloc(&d_out_data, bytes);
+    cudaMalloc(&d_out_tid, bytes);
+    
+    // Copy input array from host to device
+
+    cudaMemcpy(d_in_data, in_data, 2*bytes, cudaMemcpyHostToDevice);    
+
+    // Set up execution configuration
+    int threadsPerBlock = 32;
+    int blocksPerGrid = 1;
+    
+    // Launch the kernel
+    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+
+    //kernel warm-up
+    kernel<<<1, 1>>>(d_in_data,d_out_data,d_out_tid, N);
+
+    // Check for errors
+    err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        printf("CUDA Error: %s\n", cudaGetErrorString(err));
+        return -1;
+    }
+
+    kernel<<<blocksPerGrid, threadsPerBlock>>>(d_in_data,d_out_data,d_out_tid, N);
+        
+    // Check for errors
+    err = cudaGetLastError();
+    if (err != cudaSuccess)
+    {
+        printf("CUDA Error: %s\n", cudaGetErrorString(err));
+        return -1;
+    }
+
+    cudaMemcpy(out_data, d_out_data, bytes, cudaMemcpyDeviceToHost);
+    cudaMemcpy(out_tid, d_out_tid, bytes, cudaMemcpyDeviceToHost);
+
+    
+    // Verify results
+    for (int i = 0; i < N; i++)
+    {
+        printf("out_data[%d] = %d, processed by tid %d (%d)\n",i, out_data[i],out_tid[i],i*4+1);
+    }
+    
+
+    // Free device memory
+    cudaFree(d_in_data);
+    cudaFree(d_out_data);
+    cudaFree(d_out_tid);
+    
+    // Free host memory
+    free(in_data);
+    free(out_data);
+    free(out_tid);
+
+    
+
+    return 0;
+}
