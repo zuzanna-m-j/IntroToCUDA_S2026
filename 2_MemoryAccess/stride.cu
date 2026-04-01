3ecd5bc81724896f2d1252caec9bc14879731f5b
Zuzanna <84448044+zuzanna-m-j@users.noreply.github.com>
2026-04-01 17:16:20 -0500

    Create stride.cu

---
+++ 2_MemoryAccess/stride.cu
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <random>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+__global__ void stridedMemAccess(float *d_in, int stride){
+
+    int tid = (blockDim.x * blockIdx.x + threadIdx.x) * stride;
+    d_in[tid] = d_in[tid] + stride;
+}
+
+
+int main(){
+
+    ofstream outfile("bandwidth.data");
+
+    int blockSize = 256;
+    int gridSize = 1024 * 5;
+
+    int N = gridSize * blockSize;
+
+    printf("Processing %d fp32 elements\n", N);
+    int total_size = N * 33;
+
+    float *d_in;
+    float gpu_time;
+
+    cudaEvent_t start, stop;  
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaError_t err;
+
+
+    cudaMalloc(&d_in, total_size * sizeof(float));
+    float *h_in = (float *)malloc(total_size * sizeof(float));
+
+    for (int i = 0; i < N * 33; i++) {
+        h_in[i] = 1.0;
+    }
+
+    for (int i = 0; i < 5; i++){
+        cudaMemcpy(d_in, h_in, total_size*sizeof(float), cudaMemcpyHostToDevice);
+        stridedMemAccess<<<gridSize, blockSize>>>(d_in,0);
+    }
+
+    for (int stride = 1; stride <= 32; stride++) {
+
+        cudaMemcpy(d_in, h_in, total_size*sizeof(float), cudaMemcpyHostToDevice);
+
+        cudaEventRecord(start);
+
+        stridedMemAccess<<<gridSize, blockSize>>>(d_in,stride);
+
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&gpu_time, start, stop);
+
+        err = cudaGetLastError();
+        if (err != cudaSuccess)
+        {
+            printf("CUDA Error: %s\n", cudaGetErrorString(err));
+            return -1;
+        }
+
+        printf("Stride: %d, Bandwidth (GB/s) %f\n", stride,  N*4*2/gpu_time/1e6);
+        outfile << stride << " " << N*4*2/gpu_time/1e6 << endl;
+        
+
+    }
+    cudaFree(d_in);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    outfile.close();
+    return 0;
+
+}
